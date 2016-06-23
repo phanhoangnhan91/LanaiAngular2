7371f73fc4b740d320dea6b5854c22cf3bca5a4e
Nhan Phan <nhanphan@futurify.vn>
2016-06-23 18:34:14 +0700

    [Lanai-270] Change the startup file

--- Lanai/src/Lanai/Startup.cs
+++ Lanai/src/Lanai/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace Lanai
 {
@@ -15,6 +16,7 @@ namespace Lanai
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,6 +24,17 @@ namespace Lanai
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html"; // Put your Angular root page here 
+                    await next();
+                }
+            });
         }
     }
 }
--- Lanai/src/Lanai/wwwroot/appScripts/boot.js
+++ Lanai/src/Lanai/wwwroot/appScripts/boot.js
@@ -3,3 +3,4 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_1 = require('./app');
 platform_browser_dynamic_1.bootstrap(app_1.AppComponent);
+//# sourceMappingURL=boot.js.map
