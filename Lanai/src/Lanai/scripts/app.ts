847f0ca7ab872c3f1b8bffb33b890d89895b08c6
Nhan Phan <nhanphan@futurify.vn>
2016-06-23 18:23:56 +0700

    [Lanai-270] Change version of angular 2 to rc2

--- Lanai/src/Lanai/scripts/app.ts
+++ Lanai/src/Lanai/scripts/app.ts
@@ -1,6 +1,6 @@
 ﻿import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h3>My First Angular 2 App</h3>'
+    template: '<h3>My First Angular 2 App nhan {{1+3}}</h3>'
 })
 export class AppComponent { }
