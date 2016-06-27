eb6a6b8bcaffaa4a5df5b0166d7a81ac44bfc5fe
Nhan Phan <nhanphan@futurify.vn>
2016-06-27 14:31:16 +0700

    [Lanai-270] TMP Service

--- Lanai/src/Lanai/wwwroot/app/app.component.ts
+++ Lanai/src/Lanai/wwwroot/app/app.component.ts
@@ -1,19 +1,8 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import {Hero} from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import {HeroService} from './hero.service'
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
 @Component({
     selector: 'my-app',
@@ -81,14 +70,21 @@ const HEROES: Hero[] = [
     directives: [HeroDetailComponent]
 })
 
-export class AppComponent {
+
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
     selectedHero: Hero;
     onSelect(hero: Hero) {
         this.selectedHero = hero;
     };
-    public heroes = HEROES;
-    
-
+    heroes: Hero[]; 
+    constructor(private heroService: HeroService) {      
+    };
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    ngOnInit() {
+        this.getHeroes();
+    }
     
 }
