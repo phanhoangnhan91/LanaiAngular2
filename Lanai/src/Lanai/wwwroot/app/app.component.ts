ed6844290aa58af765ba651b00444ae194d3e6c3
Nhan Phan <nhanphan@futurify.vn>
2016-06-27 15:25:13 +0700

    [Lanai-270] Add the dashboard component

--- Lanai/src/Lanai/wwwroot/app/app.component.ts
+++ Lanai/src/Lanai/wwwroot/app/app.component.ts
@@ -2,11 +2,13 @@
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import {DashboardComponent} from  './dashboard.component';
 
 @Component({
     selector: 'my-app',
     template: `
       <h1>{{title}}</h1>
+<a [routerLink]="['Dashboard']">Dashboard</a>
   <a [routerLink]="['Heroes']">Heroes</a>
   <router-outlet></router-outlet>
   `,
@@ -21,7 +23,13 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
         path: '/heroes',
         name: 'Heroes',
         component: HeroesComponent
-    }
+        },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
 ])
 export class AppComponent {
     title = 'Tour of Heroes';
